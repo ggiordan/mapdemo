eba416d69172e142aec892f116e6104ea0fd8c0e
Geoffrey Giordano <jeff.giordano@emergencyreporting.com>
2020-12-02 08:56:38 -0500

    demo complete

--- www/js/index.js
+++ www/js/index.js
@@ -21,9 +21,55 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
+var map = null;
+
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+
+    var div = document.getElementById("page1map");
+
+    // Create a Google Maps native view under the map_canvas div.
+    map = plugin.google.maps.Map.getMap(div);
+
+    // If you click the button, do something...
+    var button = document.getElementById("button");
+    button.addEventListener("click", function () {
+
+        // Move to the position with animation
+        map.animateCamera({
+            target: { lat: 37.422359, lng: -122.084344 },
+            zoom: 17,
+            tilt: 60,
+            bearing: 140,
+            duration: 5000
+        });
+
+        // Add a maker
+        var marker = map.addMarker({
+            position: { lat: 37.422359, lng: -122.084344 },
+            title: "Welecome to \n" +
+                "Cordova GoogleMaps plugin for iOS and Android",
+            snippet: "This plugin is awesome!",
+            animation: plugin.google.maps.Animation.BOUNCE
+        });
+
+        // Show the info window
+        marker.showInfoWindow();
+    });
+
+    var setDivButton = document.getElementById("doSetDiv");
+    setDivButton.addEventListener("click", function () {
+        console.log("Click!");
+        var myDiv = map.getDiv();
+        if (myDiv) {
+            console.log("clear div");
+            map.setDiv(null);
+        }
+        else {
+            console.log("set div");
+            map.setDiv(document.getElementById("page1map"));
+        }
+    });
 }
